3713c8a28aeaff555069a05e68bea681de025f5b
Mariogs <85800261+MGS13@users.noreply.github.com>
2022-04-16 18:00:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+#1.)
+for x in range(10,71,10):
+  print(x)
+
+y=0
+#2.)
+for x in range(1,11):
+  y=x/2
+  print(y)
+num=0
+if (num > 5):
+  print("Greater than 5!") 
+
+#1
+i = 0
+while i < 5:
+  print(i)
+  i = i + 1
+#2
+i = 0
+while i < 5:
+  i = i + 1
+  print(i) 
+#3
+i = 0
+while i < 5:
+  print(i) 
+  i = i + 1
+i = 0
+while i < 5:
+  print(i)
+  i = i + 2 
+n = 5
+#While Loop
+while n > 0:
+  print("Rinse")
+  print("Lather")
+print("Dry off")
+#Outcome stopped after running 1000 steps and you crash the browser
+
+#While Loops and Counters 
+count = 0
+while count < 5:
+  print("hi!")
+  count = count + 1
+print("Good bye!") 
+count = 0
+while count < 5:
+  print("hi!")
+  count += 1
+  
+print("Good bye!") 
+password="simonsnyc"
